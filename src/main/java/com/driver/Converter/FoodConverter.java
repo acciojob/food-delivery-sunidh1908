b7cfff405f1ac85fe0645fd9cc3d9f118ca979e2
sunidh1908 <sunidh.kumar@gmail.com>
2022-12-31 11:08:08 +0530

    controller

--- src/main/java/com/driver/Converter/FoodConverter.java
+++ src/main/java/com/driver/Converter/FoodConverter.java
@@ -1,13 +1,27 @@
 package com.driver.Converter;
 
 import com.driver.io.entity.FoodEntity;
+import com.driver.model.request.FoodDetailsRequestModel;
+import com.driver.model.response.FoodDetailsResponse;
 import com.driver.shared.dto.FoodDto;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class FoodConverter {
 
     public static FoodDto EntityToDto(FoodEntity foodEntity){
         return FoodDto.builder().id(foodEntity.getId()).foodId(foodEntity.getFoodId()).
                 foodName(foodEntity.getFoodName()).foodPrice(foodEntity.getFoodPrice()).
                 foodCategory(foodEntity.getFoodCategory()).build();
+    }
+
+    public static FoodDetailsResponse DtoToResponse(FoodDto foodDto){
+        return FoodDetailsResponse.builder().foodId(foodDto.getFoodId()).foodName(foodDto.getFoodName()).
+                foodPrice(foodDto.getFoodPrice()).foodCategory(foodDto.getFoodCategory()).build();
+    }
+
+    public static FoodDetailsResponse RequestToResponse(FoodDetailsRequestModel foodDetailsRequestModel){
+        return FoodDetailsResponse.builder().foodName(foodDetailsRequestModel.getFoodName()).
+                foodPrice(foodDetailsRequestModel.getFoodPrice()).foodCategory(foodDetailsRequestModel.getFoodCategory()).build();
     }
 }
--- src/main/java/com/driver/ui/controller/FoodController.java
+++ src/main/java/com/driver/ui/controller/FoodController.java
@@ -2,11 +2,14 @@ package com.driver.ui.controller;
 
 import java.util.List;
 
+import com.driver.Converter.FoodConverter;
 import com.driver.model.request.FoodDetailsRequestModel;
 import com.driver.model.response.FoodDetailsResponse;
 import com.driver.model.response.OperationStatusModel;
 import com.driver.service.FoodService;
 import com.driver.service.impl.FoodServiceImpl;
+import com.driver.shared.dto.FoodDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,19 +30,19 @@ public class FoodController {
 
 	@GetMapping(path="/{id}")
 	public FoodDetailsResponse getFood(@PathVariable String id) throws Exception{
-		foodService.getFoodById(id);
-		return null;
+		FoodDto foodDto = foodService.getFoodById(id);
+		FoodDetailsResponse foodDetailsResponse = FoodConverter.DtoToResponse(foodDto);
+		return foodDetailsResponse;
 	}
 
 	@PostMapping("/create")
 	public FoodDetailsResponse createFood(@RequestBody FoodDetailsRequestModel foodDetails) {
-
+		//FoodDto foodDto = foodService.createFood(FoodConverter.);
 		return null;
 	}
 
 	@PutMapping(path="/{id}")
 	public FoodDetailsResponse updateFood(@PathVariable String id, @RequestBody FoodDetailsRequestModel foodDetails) throws Exception{
-
 		return null;
 	}
 
